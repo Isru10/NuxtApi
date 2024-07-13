ac553e4a27b2024bb1bd6749039da761ff71b540
Israel Sol <isrucrasus10@gmail.com>
2024-07-13 17:31:11 +0300

    nuxt components and layouts

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+
+            <h2>About</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit et corrupti animi eligendi labore provident consequatur, iusto architecto culpa deserunt nobis, unde ipsum quaerat dolorum velit asperiores impedit eos nisi?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis animi quae in quisquam officia odit impedit? Rerum voluptatum mollitia optio exercitationem quisquam. Corrupti expedita vero autem possimus cumque enim mollitia.</p>
+
+    </div> 
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+    h2{
+        margin-bottom: 20px;
+        font-size: 36px;
+    }
+    p{
+     margin: 20px 0;   
+    }
+</style>
